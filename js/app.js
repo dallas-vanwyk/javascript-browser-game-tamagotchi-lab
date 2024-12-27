84e9c77c6309fcffb1d6e942ebf0db33fc97d6aa
dallas-vanwyk <dallas.vanwyk@gmail.com>
2024-12-27 15:58:37 -0600

    initial commit with boilerplate and psuedocode

---
+++ js/app.js
@@ -0,0 +1,65 @@
+
+/*---------------------------------------------------------------------------*/
+/*-------------------------------- Notes ------------------------------------*/
+
+/* 
+It’s a one-player game.
+The player is tasked with keeping a creature happy.
+Each creature a player ‘raises’ has three main statistics.
+The stats are:
+Boredom
+Hunger
+Sleepiness
+To keep the creature happy, a player must keep three stats within a given range - greater than 0 and less than 10.
+
+Minimum Requirements
+
+[] Display three buttons when the page is initially displayed.
+[] Display three stats that randomly increment a value between 0 and 3 at a set interval.
+[] A player can click a button to set the corresponding stat to 0.
+[] Include loss logic and display a message when the player has lost.
+[] Provide a Play Again button that will reset the game.
+
+*/
+
+/*-------------------------------- Psuedocode -------------------------------*/
+
+// 1) Define the required variables used to track the state of the game.
+
+// 2) Store cached element references.
+
+// 3) Upon loading, the game state should be initialized, and a function should 
+//    be called to render this game state.
+
+// 4) The state of the game should be rendered to the user.
+
+// 5) Handle the game over logic. 
+
+// 6) Handle each instance of a player clicking a button with the use of a 
+//    `handleClick()` function.
+
+// 7) Create reset functionality.
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const playButtonEl = document.querySelector('#play');
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
